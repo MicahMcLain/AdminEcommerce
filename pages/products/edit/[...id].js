3ad848482232579db72753adaff3b8570f34263a
MicahMcLain <mclainmicah1@gmail.com>
2023-11-08 12:03:08 -0500

    finished pages and functions for editing existing products. added page and functions to delete product. all work

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -1,9 +1,11 @@
 import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function EditProductPage() {
+    const [productInfo, setProductInfo] = useState(null);
     const router = useRouter();
     const {id} = router.query;
     useEffect(()=>{
@@ -11,12 +13,16 @@ export default function EditProductPage() {
             return;
         }
         axios.get('/api/products?id='+id).then(response => {
-            console.log(response.data);
+            setProductInfo(response.data);
         })
     },[id])
     return (
         <Layout>
-            Edit product form here
+            <h1>Edit Product</h1>
+            {productInfo && (
+                <ProductForm {...productInfo} />
+            )}
+           
         </Layout>
     );
 }
